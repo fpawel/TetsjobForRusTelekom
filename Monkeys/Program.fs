d38b5e3c10e0b4938c9ca8af282bc998290e8d7a
binf <binf@DESKTOP-R9ER2U0>
2016-05-26 19:56:10 +0300

    fix

--- Monkeys/Program.fs
+++ Monkeys/Program.fs
@@ -26,19 +26,40 @@ let calc xs =
         |> (=) (Some true)  )
 
 
+let print xs = 
+    let s1 = xs |> List.fold (fun a (x,d) -> 
+        sprintf "%s(%d,%d)" (if a="" then "" else a + " ") x d ) ""
+    calc xs
+    |> function true -> "YES" | _ -> "NO"
+    |> printfn "%d обезьян %s - %s" (List.length xs) s1 
+
+let rec loop() = 
+    printf "введите количество обезьян или 'q'>"
+    let s = Console.ReadLine().Trim()
+    if s = "q" then () else 
+    let b,n = Int32.TryParse( s )
+    if b then
+        readThrows [] 0 n 
+        |> List.rev        
+        |> print  
+    else
+        printf "некорректный ввод %A" s
+    loop()
+
+
 
 
 [<EntryPoint>]
 do
-    
+    print [ (0,1); (1,-1) ]
+    print [ (0,1); (1,1); (2,-2) ]
+    print [ (2,-10); (3,-10); (0,5); (5,-5); (10,1) ]
     while true do
         printf "введите количество обезьян >"
         let n = Int32.Parse( Console.ReadLine().Trim() )
         readThrows [] 0 n 
         |> List.rev        
-        |> calc
-        |> function true -> "YES" | _ -> "NO"
-        |> printfn "ответ - %s"
+        |> print 
 
     Console.ReadKey() |> ignore
 
